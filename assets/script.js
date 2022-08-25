5478331e92ce5838b4ca562c48d80341a583bd0e
edgarrobles1234 <111394585+edgarrobles1234@users.noreply.github.com>
2022-08-24 22:24:29 -0700

    javascript functions added to the project

--- assets/script.js
+++ assets/script.js
@@ -1,29 +1,62 @@
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
+function listTopics(){
 for (var x = 0; x < topics.length; x++) {
  console.log(topics[x]);
 }
+}
 
-/*if (topic === 'HTML') {
+function selectTopic(){
+if (randomTopic === 'HTML') {
   console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
+} else if (randomTopic === 'CSS') {
   console.log("Let's study CSS!");
-} else if (topic === 'Git') {
+} else if (randomTopic === 'Git') {
   console.log("Let's study Git!");
-} else if (topic === 'JavaScript') {
+} else if (randomTopic === 'JavaScript') {
   console.log("Let's study JavaScript!");
 } else {
   console.log('Please try again!');
+}
+}
+
+console.log('Here are the topics we learned through Prework:');
+listTopics();
+console.log('Which topic should we study first?');
+selectTopic();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*function helloWorld() {
+  console.log("Hello, world, I am a function!")
+ }
+ 
+for(var x=0; x<5; x++) {
+  helloWorld();
+  console.log(x+1,"times!");
 }*/
 
-var shapes=["triangle","square","pentagon","hexagon","circle","you"]
+
+/*var shapes=["triangle","square","pentagon","hexagon","circle","you"];
 console.log("Thses are some of my favorite shapes");
 
 for(var x = 0; x < shapes.length; x++) {
     console.log("Go",shapes[x],"go!");
    }
-
-var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
 
 
 
@@ -34,4 +67,4 @@ if(studentInfo[1]==deadlyage){
 console.log(studentInfo[0], "is deadly.");
 }else{
     console.log(studentInfo[0], "is bologna.");
-}
+}*/
